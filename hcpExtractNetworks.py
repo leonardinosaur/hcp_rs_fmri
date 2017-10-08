a8d1468cac4389df857bdb7933e2416968b74ca3
Leonar(Dino) Digma <leonardinodigma@ucsd.edu>
2017-10-07 19:49:31 -0700

    small touches - almost ready

--- hcpExtractNetworks.py
+++ hcpExtractNetworks.py
@@ -7,17 +7,17 @@ A script to set-up a *.sh job that can be submitted to
 	SGE to run hcpExtractNetwork.py on multiple datasets
 	in parallel. After running, this script will print out
 	the command that you can run from $HOME/extractjobs/
-	to submit the *.sh job for processing. A sample command
-	looks something like:
-
-	submit -s job_extract_network_5.sh -f job_extract_network_5.inputs -o job_extract_network_5.options
+	to submit the *.sh job for processing.
 
 Numpy arrays will be saved with randomly generated filenames (e.g. 
-	tmp_aSdf42.npy). This was implemented since may input files will likely
-	have identical file names. An accompanying textfile (e.g.
+	tmp_aSdf42.npy). This was implemented since many input files will likely
+	have identical file names and to take burden off user from having to specify
+	output file names. After the numpy array is generated, an accompanying textfile (e.g.
 	tmp_aSdf42.txt) will be saved along with the array. This text files
 	lists which inputs were used to generate the numpy array. This text file
 	can be easily parsed to rename output .npy files.
+  
+For further information, please see hcpExtractNetwork.py
 
 
 --input
@@ -33,19 +33,19 @@ Directory where connectivity matrices will be saved
 A path to a 3D segmentation volume. This segmentation will be applied to
 	all of the 4D volumes specified in INPUT
 
-
 --bmasks
 
-Not yet implemented
+Not yet implemented for batch processing.
+This option has been implemented for single case processing so
+	please refer to hcpExtractNetwork
 
 
 Written on 27 Sept 2017 by Dino
-Last updated on 3 Oct 2017 by Dino
+Last updated on 7 Oct 2017 by Dino
 	-Set-up SGE stuff
 
 To-do:
-	-The options file is always the same so we don't
-		have to remake it everytime
+-Remove unused imports
 
 """
 import argparse
@@ -86,7 +86,6 @@ if not os.path.exists(sge_dir + 'output.options'):
 prev_jobs = glob(sge_dir + 'job_extract_network*.sh')
 job_name = 'job_extract_network_%s.sh' % (str(len(prev_jobs)))
 job_inps = 'job_extract_network_%s.inputs' % (str(len(prev_jobs)))
-job_opts = 'job_extract_network_%s.options' % (str(len(prev_jobs)))
 
 # Create text file for list of subjects
 shutil.copy(args.input, sge_dir + '/' + job_inps) 
@@ -100,7 +99,6 @@ with open(sge_dir + job_name, 'w') as w:
 		w.write(cmd)
 	elif args.bmasks:
 		sys.exit('Not yet implemented')
-
 	else:
 		sys.exit('Error: need to specify segmentation(s) with -a or -b')
 
